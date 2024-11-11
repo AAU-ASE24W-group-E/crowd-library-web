54ccf17a088f2188789b4efabd3ca2ccce753f7b
djamn <david@jamnig.net>
2024-11-11 01:09:03 +0100

    added basic file and removed useless tutorial from app.vue

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Template represents html -->
+  <div class="text-center">
+    <h1 class="text-green-600">{{ message }}</h1>
+    <button class="bg-green-700 hover:bg-green-800 text-white p-3 rounded cursor-pointer border-none" @click="updateMessage">Klick mich</button>
+  </div>
+</template>
+
+<script setup>
+// Logic of the component gets defined (like in angular the .ts file of the component)
+import { ref } from 'vue';
+
+// Reactive variable
+const message = ref('Hallo, Vue11!');
+
+// Function that changes the value of a message
+const updateMessage = () => {
+  message.value = 'Die Nachricht wurde aktualisiert!';
+};
+</script>
+
+<style scoped>
+/*Styling related stuff - Will not be used since we use tailwind */
+</style>
