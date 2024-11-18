e92b75175017f9c76f4dba73bff14a2d175ab39e
djamn <david@jamnig.net>
2024-11-18 20:36:49 +0100

    Added some custom colors for darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'selector',
+  // darkMode: ['variant', '&:not(.light *)'], // force darkmode until we have a proper selector
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-mode': '#050505',
+        'dark-mode-inside': '#1f2937',
+        'dark-mode-inside-hover': '#2a3748',
+        'title-dark-mode-text': '#c7c7c7',
+        'dark-mode-text': '#909090',
+      },
+
+    },
   },
   variants: {
     extend: {},
