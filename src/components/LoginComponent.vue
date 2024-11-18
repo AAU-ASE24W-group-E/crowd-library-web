9430d25241174b16246284cca8cd71d6c9ad2519
djamn <david@jamnig.net>
2024-11-19 00:19:17 +0100

    implemented first login structure

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="tw-component-container">
-    Works
+  <div class="flex items-center justify-center">
+    <div
+      class="flex justify-start flex-col items-center relative rounded-lg w-[450px] max-sm:w-11/12 h-auto px-4 bg-white dark:bg-gray-700">
+      <div class="tw-heading mt-2">Login</div>
+      <form
+        @submit.prevent="login"
+        class="flex flex-col items-center w-full h-full mb-3"
+      >
+        <div class="w-full mb-6">
+          <label for="username-email" class="tw-input-label">
+            LOGIN.USERNAME_OR_EMAIL_LABEL
+          </label>
+          <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+              <font-awesome-icon class="tw-icon text-gray-500 dark:text-gray-300" :icon="faEnvelope"/>
+            </div>
+            <input
+              class="tw-input"
+              id="username-email"
+              autocomplete="username"
+              type="text"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 <script setup>
 
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,7 @@ import LoginComponent from "@/components/LoginComponent.vue";
 </script>
 
 <template>
-  <main class="tw-page-container">
+  <main class="tw-page-container justify-center bg-gray-300 dark:bg-dark-mode">
     <LoginComponent/>
   </main>
 </template>
