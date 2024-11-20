7102f6842e772c1252d33aef16547e5d07482fdb
djamn <david@jamnig.net>
2024-11-20 12:56:26 +0100

    added files for register implementation

---
+++ src/components/RegisterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div
+      class="flex justify-start flex-col items-center relative rounded-lg w-[450px] max-sm:w-11/12 h-auto px-4 bg-white dark:bg-gray-700 ">
+      <div class="tw-heading mb-5 mt-2 ">Register</div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faEnvelope, faEye, faEyeSlash, faLock} from "@fortawesome/free-solid-svg-icons";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {reactive, ref} from "vue";
+import {useRouter} from "vue-router";
+
+library.add(faEye, faEyeSlash); // TODO
+</script>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RegisterComponent from "@/components/RegisterComponent.vue";
+</script>
+
+<template>
+  <main class="tw-page-container justify-center bg-gray-300 dark:bg-dark-mode">
+    <RegisterComponent/>
+  </main>
+</template>
