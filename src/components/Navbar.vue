6605be78d623ac5d451270b71f43d71ee2f82b37
djamn <david@jamnig.net>
2024-12-22 23:45:37 +0100

    added file structure for navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="tw-component-container dark:bg-dark-mode-inside rounded-lg py-3 mt-2 h-16 border-1"
+  >
+    <!--Browser-->
+    <div class="w-full h-full flex items-center justify-between">
+      <router-link to="/">
+        <img
+          :src="config.NAVBAR_LOGO_SRC"
+          alt="logo"
+          class="h-8 w-40 object-contain pr-8 max-lg:p-0 decoration-0"
+        />
+      </router-link>
+      <!--      TODO-->
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+//
+import {config} from "@vue/test-utils";
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HomeComponent from "@/components/HomeComponent.vue";
+import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <main class="tw-page-container">
+    <Navbar/>
     <HomeComponent/>
   </main>
 </template>
