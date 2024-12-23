5b26b302630eee6d379c51d468025bb5c071a9f9
djamn <david@jamnig.net>
2024-12-23 10:29:34 +0100

    added first structure for navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,39 @@
 <template>
   <div
-    class="tw-component-container dark:bg-dark-mode-inside rounded-lg py-3 mt-2 h-16 border-1"
+    class="tw-component-container dark:bg-dark-mode-inside rounded-lg py-5 mt-2 h-20 bg-blue-100"
   >
     <!--Browser-->
     <div class="w-full h-full flex items-center justify-between">
       <router-link to="/">
         <img
-          :src="config.NAVBAR_LOGO_SRC"
+          src="../assets/logo_simple.png"
           alt="logo"
-          class="h-8 w-40 object-contain pr-8 max-lg:p-0 decoration-0"
+          class="w-20 h-20 mr-12 object-contain pr-8 decoration-0"
         />
       </router-link>
-      <!--      TODO-->
+      <nav
+        class="lg:flex-1 lg:flex lg:items-center lg:flex-row lg:justify-start max-lg:flex max-lg:items-start max-lg:self-start max-lg:flex-col w-full"
+      >
+        <div class="relative w-full">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <font-awesome-icon
+              class="tw-icon text-gray-500 dark:text-gray-300"
+              :icon="faSearch"
+            />
+          </div>
+          <input class="tw-input w-full rounded-3xl h-9" :placeholder="'Search by title, author, ISBN...'">
+        </div>
+      </nav>
+
+      <div class="flex justify-end max-lg:justify-end ml-10">
+        <!--       TODO-->
+      </div>
+
     </div>
   </div>
 </template>
 
-
 <script setup>
-//
-import {config} from "@vue/test-utils";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 </script>
