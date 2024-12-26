410e6c7daff09e827f0da140d11dba78b7016469
djamn <david@jamnig.net>
2024-12-26 23:08:41 +0100

    added structure for book search list

---
+++ src/components/BookSearchList.vue
@@ -0,0 +1,11 @@
+<template>
+  <BookEntry></BookEntry>
+  <BookEntry></BookEntry>
+  <BookEntry></BookEntry>
+</template>
+
+
+<script setup>
+
+import BookEntry from "@/components/BookEntry.vue";
+</script>
---
+++ src/views/BookSearchList.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Footer from "@/components/Footer.vue";
+import BookEntry from "@/components/BookEntry.vue";
+import Navbar from "@/components/navbar/Navbar.vue";
+</script>
+
+<template>
+  <main class="tw-page-container">
+    <Navbar/>
+    <BookSearchList/>
+    <div class="tw-footer-margin"></div>
+    <Footer/>
+  </main>
+</template>
