399a7b82c79bf7d2edd37e5c0a4654f85383edb1
djamn <david@jamnig.net>
2024-12-26 21:37:42 +0100

    added navbar buttons when not logged in

--- src/components/navbar/NavbarButtons.vue
+++ src/components/navbar/NavbarButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center flex-row justify-between space-x-6 max-lg:space-x-3 max-sm:space-x-6"
+  <div class="flex items-center flex-row justify-between space-x-6 max-lg:space-x-5 max-sm:space-x-6"
        ref="dropdownRef">
     <div class="tw-navbar-dropdown-container">
       <button
@@ -26,52 +26,68 @@
       >
         <font-awesome-icon class="tw-icon text-2xl" :icon="faCircleUser"/>
       </button>
+<!--      TODO dynamic width-->
       <div v-if="dropdownAccountOpen" class="-right-4 w-44 tw-dropdown-inner-layout">
-        <a
-          class="px-2 py-2 text-base text-gray-700 dark:text-title-dark-mode-text flex items-center cursor-default"
-        >
+        <div v-if="loggedIn">
+          <a
+            class="px-2 py-2 text-base text-gray-700 dark:text-title-dark-mode-text flex items-center cursor-default"
+          >
         <span class="flex flex-row">
           <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faUser"></font-awesome-icon>
           <span class="font-medium"> {{ username }}</span>
         </span>
-        </a>
+          </a>
 
-        <div class="tw-dropdown-separator"></div>
+          <div class="tw-dropdown-separator"></div>
 
-        <router-link to="/account-settings" class="tw-dropdown-inner-action-layout">
-          <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faGear"></font-awesome-icon>
-          Account Settings
-        </router-link>
+          <router-link to="/account-settings" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faGear"></font-awesome-icon>
+            Account Settings
+          </router-link>
 
-        <div class="tw-dropdown-separator"></div>
+          <div class="tw-dropdown-separator"></div>
 
-        <router-link to="/location" class="tw-dropdown-inner-action-layout">
-          <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faLocationDot"></font-awesome-icon>
-          My Location
-        </router-link>
+          <router-link to="/location" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faLocationDot"></font-awesome-icon>
+            My Location
+          </router-link>
 
-        <div class="tw-dropdown-separator"></div>
+          <div class="tw-dropdown-separator"></div>
 
-        <router-link to="/wishlist" class="tw-dropdown-inner-action-layout">
-          <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faHeart"></font-awesome-icon>
-          My Wishlist
-        </router-link>
+          <router-link to="/wishlist" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faHeart"></font-awesome-icon>
+            My Wishlist
+          </router-link>
 
-        <div class="tw-dropdown-separator"></div>
+          <div class="tw-dropdown-separator"></div>
 
-        <router-link to="/bookish-map" class="tw-dropdown-inner-action-layout">
-          <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faMap"></font-awesome-icon>
-          Bookish Map
-        </router-link>
+          <router-link to="/bookish-map" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faMap"></font-awesome-icon>
+            Bookish Map
+          </router-link>
 
-        <div class="tw-dropdown-separator"></div>
+          <div class="tw-dropdown-separator"></div>
 
-        <a @click="logout" class="tw-dropdown-inner-action-layout">
-          <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faRightFromBracket"></font-awesome-icon>
-          Logout
-        </a>
+          <a @click="logout" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faRightFromBracket"></font-awesome-icon>
+            Logout
+          </a>
+        </div>
+        <div v-if="!loggedIn">
+          <router-link to="/login" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faRightToBracket"></font-awesome-icon>
+            Login
+          </router-link>
+
+          <div class="tw-dropdown-separator"></div>
+          <router-link to="/register" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon class="tw-navbar-dropdown-icon" :icon="faUser"></font-awesome-icon>
+            Register
+          </router-link>
+
+          <div class="tw-dropdown-separator"></div>
+        </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -85,7 +101,7 @@ import {
   faHeart,
   faLocationDot,
   faMap,
-  faRightFromBracket,
+  faRightFromBracket, faRightToBracket,
   faUser
 } from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
@@ -93,6 +109,7 @@ import {onMounted, onUnmounted, ref} from "vue";
 
 let dropdownAccountOpen = ref(false);
 let username = 'undefined';
+let loggedIn = ref(false);
 const dropdownRef = ref(null);
 
 const handleAccountClick = () => {
