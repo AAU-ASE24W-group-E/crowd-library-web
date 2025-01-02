bfac7e9d87a51326198d1781807626bceefa187f
djamn <david@jamnig.net>
2025-01-03 00:53:19 +0100

    added dropdown handling for booklist buttons

--- src/components/BookEntry.vue
+++ src/components/BookEntry.vue
@@ -1,13 +1,14 @@
 <template>
   <div
-    class="tw-component-container w-full bg-white dark:bg-dark-mode-inside shadow-md rounded-lg border border-gray-200 dark:border-gray-600 transition duration-500 ease-in-out hover:scale-101 animate-fade-in">
+    @click="toggleDropdown"
+    class="tw-component-container w-full bg-white dark:bg-dark-mode-inside shadow-md rounded-lg border border-gray-200 dark:border-gray-600 transition duration-500 ease-in-out hover:scale-101 animate-fade-in border-1 relative">
     <div class="flex flex-row w-full">
       <img
         src="../assets/logo_simple.png"
         alt="logo"
         class="object-contain decoration-0 mr-3"
       />
-      <div class="flex flex-col">
+      <div class="flex flex-col flex-grow">
         <span
           class="text-xl font-semibold dark:text-title-dark-mode-text">{{ book.title }} ({{
             book.year
@@ -28,8 +29,6 @@
             <span class="tw-book-entry-info-title">Status: <span
               :class="{'text-green-500': book.isAvailable, 'text-red-500': !book.isAvailable}">
               {{ book.status }}</span></span>
-            <!--            <span>Availability</span>-->
-            <!--            <span>Due Date</span> -->
           </div>
           <div v-if="book.isAvailable" class="flex flex-col">
              <span class="tw-book-entry-info-title">Lendable:
@@ -54,17 +53,27 @@
           </div>
         </div>
       </div>
+      <div class="flex items-center justify-center ml-2">
+        <font-awesome-icon :icon="faChevronRight" class="tw-icon"
+                           :class="{ 'rotate-90': dropdownOpen }"></font-awesome-icon>
+      </div>
     </div>
-    <div class="flex flex-row w-full mt-2 space-x-20 ml-48 max-sm:space-x-8 max-sm:justify-center max-sm:ml-0">
-      <button v-if="!isWishlist" class="btn-primary btn-gray rounded-2xl">Add to wishlist</button>
+    <div
+      v-if="dropdownOpen"
+      class="flex flex-row w-full mt-2 space-x-20 ml-48 max-sm:space-x-8 max-sm:justify-center max-sm:ml-0"
+    >
+      <button v-if="!isWishlist" class="btn-primary btn-gray rounded-2xl">
+        Add to wishlist
+      </button>
       <button class="btn-primary btn-green rounded-2xl">Send request</button>
     </div>
   </div>
 </template>
 
-
 <script setup>
-// TODO delete button in wishlist
+import {ref} from "vue";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
 
 defineProps({
   book: {
@@ -77,6 +86,11 @@ defineProps({
   },
 });
 
+const dropdownOpen = ref(false);
+
+function toggleDropdown() {
+  dropdownOpen.value = !dropdownOpen.value;
+}
 </script>
 
 <style>
