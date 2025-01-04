31ee04bf8523a97ecaf0b44d57e63378c90219ce
djamn <david@jamnig.net>
2025-01-05 00:37:11 +0100

    fix

--- tests/Books/BookSearchList.spec.js
+++ tests/Books/BookSearchList.spec.js
@@ -1,7 +1,6 @@
 import {mount} from '@vue/test-utils';
 import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
 import BookSearchList from "@/components/BookSearchList.vue";
-import BookEntry from "@/components/BookEntry.vue";
 
 
 const push = vi.fn();
@@ -34,7 +33,7 @@ describe('BookEntry', () => {
 
   it("filters books based on selected categories", async () => {
     await wrapper.vm.selectedCategories.push("Distance");
-    await wrapper.vm.handleSelection({ target: { value: "Distance" } });
+    await wrapper.vm.handleSelection({target: {value: "Distance"}});
 
     expect(wrapper.vm.selectedCategories).toContain("Distance");
     expect(wrapper.vm.books).toBeTruthy();
@@ -44,7 +43,7 @@ describe('BookEntry', () => {
     wrapper.vm.dropdownSortOpen = true;
     await wrapper.vm.$nextTick();
 
-    const clickEvent = new MouseEvent("click", { bubbles: true });
+    const clickEvent = new MouseEvent("click", {bubbles: true});
     document.dispatchEvent(clickEvent);
 
     expect(wrapper.vm.dropdownSortOpen).toBe(false);
@@ -62,36 +61,25 @@ describe('BookEntry', () => {
   });
 
   it("adds and then removes a category correctly", async () => {
-    // Ensure initial state
     expect(wrapper.vm.selectedCategories).toHaveLength(0);
     expect(wrapper.vm.categories).toContain("Title");
 
-    // Simulate adding a category
     const selectElement = wrapper.find("#version-category");
     await selectElement.setValue("Title");
-    await selectElement.trigger("change"); // Trigger the change event
+    await selectElement.trigger("change");
 
-    // Wait for the DOM to update
     await wrapper.vm.$nextTick();
 
-    // Assert that the category has been added to selectedCategories
     expect(wrapper.vm.selectedCategories).toContain("Title");
     expect(wrapper.vm.categories).not.toContain("Title");
 
-    // Find the 'x' icon for the added category
     const removeButton = wrapper.find("#remove-category");
     expect(removeButton.exists()).toBe(true); // Ensure the button exists
 
-    // Trigger the click event to remove the category
     await removeButton.trigger("click");
-
-    // Wait for the DOM to update
     await wrapper.vm.$nextTick();
 
-    // Assert that the category has been removed from selectedCategories
     expect(wrapper.vm.selectedCategories).not.toContain("Title");
     expect(wrapper.vm.categories).toContain("Title");
   });
-
-
 });
