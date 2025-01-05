67213ac1bdbccd62a5b845febce56ddd81b727dd
elikonrad-aau <elikonrad@edu.aau.at>
2025-01-05 13:49:48 +0100

    Cypress config + home component test

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,7 @@
-// https://on.cypress.io/api
 
-describe('My First Test', () => {
+describe('Main test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('h1', 'Welcome to Crowd Library!')
   })
 })
