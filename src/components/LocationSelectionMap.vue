787ca5e2017ec56f522169e355afdd5b3bcf56ac
djamn <david@jamnig.net>
2025-01-09 01:15:18 +0100

    added setting of marker

--- src/components/LocationSelectionMap.vue
+++ src/components/LocationSelectionMap.vue
@@ -1,16 +1,52 @@
 <template>
-  <div id="map-container" class="w-full h-[300px] rounded-lg relative">
-    <div id="map" class="w-full h-full rounded-lg"></div>
+  <div id="map-container" class="w-full h-full rounded-lg relative">
+    <div id="map" class="w-full h-[300px] rounded-lg"></div>
+<!--    <div class="absolute top-2 left-2 z-10 bg-white p-2 rounded shadow">-->
+    <div>
+      <p v-if="clickedLocation">Lat: {{ clickedLocation.lat.toFixed(5) }}, Lng: {{ clickedLocation.lng.toFixed(5) }}</p>
+      <button
+        @click="resetMarker"
+        class="mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+      >
+        Reset Marker
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
-import L, { Map, Marker } from "leaflet";
+import { ref, onMounted } from "vue";
+import L, { type LeafletMouseEvent, Map, Marker } from "leaflet";
 import "leaflet-control-geocoder";
 
+const clickedLocation = ref<{ lat: number; lng: number } | null>(null);
+
 let map: Map;
-let marker: Marker;
+let marker: Marker | null = null;
+
+const onMapClick = (e: LeafletMouseEvent) => {
+  const { lat, lng } = e.latlng;
+
+  clickedLocation.value = { lat, lng };
+
+  if (marker) {
+    map.removeLayer(marker);
+  }
+
+  marker = L.marker([lat, lng])
+    .addTo(map)
+    // .bindPopup(`Lat: ${lat.toFixed(5)}, Lng: ${lng.toFixed(5)}`)
+    .bindPopup(`Your selected location`)
+    .openPopup();
+};
+
+const resetMarker = () => {
+  if (marker) {
+    map.removeLayer(marker);
+    marker = null;
+    clickedLocation.value = null;
+  }
+};
 
 onMounted(() => {
   map = L.map("map").setView([46.625, 14.306], 13);
@@ -20,8 +56,13 @@ onMounted(() => {
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  // TODO remove
-  marker = L.marker([46.625, 14.306])
+  // Initial marker
+  L.circleMarker([46.625, 14.306], {
+    radius: 3,
+    color: "red",
+    fillColor: "red",
+    fillOpacity: 1,
+  })
     .addTo(map)
     .bindPopup("This is your location.")
     .openPopup();
@@ -33,9 +74,15 @@ onMounted(() => {
   geocoder.on("markgeocode", (e: any) => {
     const { center, name } = e.geocode;
     map.setView(center, 13);
-    marker.setLatLng(center).bindPopup(name).openPopup();
+    if (marker) {
+      map.removeLayer(marker);
+    }
+    marker = L.marker(center).addTo(map).bindPopup(name).openPopup();
+    clickedLocation.value = { lat: center.lat, lng: center.lng };
   });
 
   geocoder.addTo(map);
+
+  map.on("click", onMapClick);
 });
 </script>
