96bf7aea51d1d13796548a850aa0fd6bbb067b87
Selina Isak <seisak@edu.aau.at>
2025-01-12 16:03:32 +0100

    Changed styling and put it in main.css

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -1,34 +1,33 @@
 <template>
     <div v-show="isActive" class="tab">
-      <slot></slot> <!-- Use slot to insert content later when used -->
+        <slot></slot> <!-- Use slot to insert content later when used -->
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     props: {
-      name: {
-        type: String,
-        required: true
-      },
-      title: {
-        type: String,
-        required: true
-      }
+        name: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        }
     },
     computed: {
-      /* Check if this tab is active by comparing index with activeTab from Tabs Component */
-      isActive() {
-        return this.$parent.activeTab === this.index;
-      }
+        /* Check if this tab is active by comparing index with activeTab from Tabs Component */
+        isActive() {
+            return this.$parent.activeTab === this.index;
+        }
     },
     created() {
-      this.index = this.$parent.tabs.length;
-      this.$parent.registerTab({
-        title: this.title,
-        name: this.name
-      });
+        this.index = this.$parent.tabs.length;
+        this.$parent.registerTab({
+            title: this.title,
+            name: this.name
+        });
     }
-  };
-  </script>
-  
+};
+</script>
