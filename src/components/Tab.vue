dfd93dc552fc13a01bff71d439136e51cc86c88a
Selina Isak <seisak@edu.aau.at>
2025-01-15 00:38:29 +0100

    Fix bug in Tab.vue, to ensure parent component is available

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -23,15 +23,22 @@ export default {
         }
     },
     created() {
-        this.index = this.$parent.tabs.length;
-        this.$parent.registerTab({
-            title: this.title,
-            name: this.name
-        });
-        if(this.index == 0){
-            this.$parent.activateTab(this.index);
+        if (this.$parent && this.$parent.tabs) {
+            // Ensure $parent (=Tabs component) and tabs array are available
+            this.index = this.$parent.tabs.length;
+            this.$parent.registerTab({
+                title: this.title,
+                name: this.name
+            });
+
+            if (this.index === 0) {
+                this.$parent.activateTab(this.index);
+            }
+
+            console.log("Registered " + this.title);
+        } else {
+            console.warn('Parent (=Tabs) component not available yet');
         }
-        console.log("Registered " + this.title);
     }
 };
 </script>
