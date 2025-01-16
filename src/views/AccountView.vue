4e08c2aadff558839dfc6c45ff4c37c090924cf3
julian <julian.lechner@aau.at>
2025-01-16 13:01:00 +0100

    Removed edit location
    this will display user information like email or username and provide the possibility to change them.

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -2,16 +2,13 @@
 import AccountComponent from "@/components/AccountComponent.vue";
 import Navbar from "@/components/navbar/Navbar.vue";
 import Footer from "@/components/Footer.vue";
-import LocationSetting from '@/components/LocationSetting.vue'
-import LocationEdit from '@/components/LocationEdit.vue'
+
 </script>
 
 <template>
   <main class="tw-page-container">
     <Navbar/>
     <AccountComponent/>
-<!--    <LocationSetting :isInSettings="true"/>-->
-    <LocationEdit/>
     <div class="tw-footer-margin"></div>
     <Footer/>
   </main>
