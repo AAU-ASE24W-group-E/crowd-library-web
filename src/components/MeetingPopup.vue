ecaf9dbc6ea4c1299fdbb56eb1f6a2b723b2c1ab
Selina Isak <seisak@edu.aau.at>
2025-01-17 21:36:17 +0100

    Add place input for meeting popup

--- src/components/MeetingPopup.vue
+++ src/components/MeetingPopup.vue
@@ -7,6 +7,11 @@
                 <div class="modal-header">
                     <h2 class="title">Send a Meeting Suggestion to {{ request.from }}</h2>
                 </div>
+                <div class="input-field mt-4">
+                    <label for="place" class="block text-sm font-medium">Place</label>
+                    <input id="place" type="text" placeholder="Enter place"
+                        class="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </div>
                 <div class="flex space-x-8 mt-4">
                     <button @click="hide" class="btn-primary btn-gray rounded-2xl">Cancel</button>
                     <button @click="hide" class="btn-primary btn-green rounded-2xl">Send Meeting Request</button>
