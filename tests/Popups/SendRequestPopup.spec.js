28c25d7208c87b91610eec08222921a38867fdcc
Selina Isak <seisak@edu.aau.at>
2025-01-18 18:10:01 +0100

    Init test file for SendRequestPopup component

---
+++ tests/Popups/SendRequestPopup.spec.js
@@ -0,0 +1,38 @@
+import { mount } from '@vue/test-utils';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import SendRequestPopup from '@/components/SendRequestPopup.vue';
+
+const push = vi.fn();
+
+vi.mock('vue-router', () => ({
+  useRouter: () => ({
+    push,
+  }),
+}));
+
+describe('SendRequestPopup', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(SendRequestPopup, {
+      props: {
+        book: {
+            title: 'The Forgotten Forest',
+            year: '2015',
+            author: 'Alice Morningstar',
+            publisher: 'Whispering Pines',
+            format: 'Paperback',
+            language: 'EN',
+            ISBN: '1122334455',
+            owner: 'User1',
+            lat: 46.617415,
+            long: 14.263625,
+        }
+      }
+    });
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+});
