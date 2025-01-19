6f3d32f5a457effff3a1f2c52df662aa394b1aa9
djamn <david@jamnig.net>
2025-01-19 17:16:04 +0100

    updating location in store when editing location

--- src/components/LocationEdit.vue
+++ src/components/LocationEdit.vue
@@ -43,7 +43,7 @@ import LocationSelectionMap from '@/components/LocationSelectionMap.vue';
 import { Snackbar } from '@/utils/snackbar.ts';
 import { SnackbarType } from '@/enums/snackbar.ts';
 import { userService } from '@/services/UserService.ts';
-import { useUserStore } from '@/stores/user.ts'
+import { useUserStore } from '@/stores/user.ts';
 
 const isLoading = ref(false);
 const selectedLocation = ref<{ lat: number; lng: number } | null>(null);
@@ -76,12 +76,11 @@ const handleLocationEdit = async () => {
       longitude: selectedLocation.value.lng,
     };
 
-    // TODO -DONE uid has to be set
-
-    await userService.updateLocation(userStore.user?.id ?? "", payload);
+    await userService.updateLocation(userStore.user?.id ?? '', payload);
+    userStore.setAddress(payload.latitude, payload.longitude);
 
     Snackbar.showSnackbar('Location was set successfully', SnackbarType.SUCCESS);
-    await router.push('/login');
+    await router.push('/');
   } catch (e) {
     Snackbar.showSnackbar(
       'There was an error setting the location, check out console',
