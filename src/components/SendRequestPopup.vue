5cbc8d0e104253435f3246b31d2c41ab8d24fe63
djamn <david@jamnig.net>
2025-01-20 22:59:26 +0100

    implemented basic request book possibility

--- src/components/SendRequestPopup.vue
+++ src/components/SendRequestPopup.vue
@@ -4,10 +4,11 @@
         <div v-if="isVisible" class="tw-component-container popup-overlay" @click="hide">
             <div class="popup-content" @click.stop>
                 <button @click="hide" class="popup-close-btn" id="closeBtn">X</button>
-                <div>
-                    <h1 class="popup-title">Send a Request to {{ book.owner }}</h1>
-                </div>
-                <div class="input-field mt-4">
+              <div class="flex items-center flex-col space-y-3">
+                <h1 class="popup-title">Send a Request to {{ book.owner }}</h1>
+                <span class="dark:text-dark-mode-text">Are you sure that you want to send a request to get this book?</span>
+              </div>
+<!--                <div class="input-field mt-4">
                     <label for="request-purpose" class="popup-label">I want to request this book for ...</label>
                     <select
                         id="request-purpose"
@@ -16,10 +17,10 @@
                         <option v-if="book.isExchangeable" value="exchanging">Exchanging</option>
                         <option v-if="book.isGiftable" value="gifting">Gifting</option>
                     </select>
-                </div>
+                </div>-->
                 <div class="flex justify-center space-x-4 mt-4">
                     <button @click="hide" class="btn-primary btn-gray rounded-2xl" id="cancelBtn">Cancel</button>
-                    <button @click="hide" class="btn-primary btn-green rounded-2xl" id="requestBookBtn">Request Book</button>
+                    <button @click="requestBook" class="btn-primary btn-green rounded-2xl" id="requestBookBtn">Request Book</button>
                 </div>
             </div>
         </div>
@@ -28,8 +29,14 @@
 
 <script setup>
 import { ref } from 'vue';
+import { lendingService } from '@/services/LendingService.ts';
+import { LendingStatus } from '@/enums/lendingStatus.ts';
+import { useUserStore } from '@/stores/user.ts';
+import { Snackbar } from '@/utils/snackbar.ts';
+import { SnackbarType } from '@/enums/snackbar.ts';
 
 const isVisible = ref(false);
+const userStore = useUserStore();
 
 const props = defineProps({
     book: {
@@ -42,12 +49,34 @@ const props = defineProps({
 const show = () => {
     isVisible.value = true;
     console.log("Popup show called");
+  console.log(props.book);
 };
 
 // Function to hide the popup
 const hide = () => {
     isVisible.value = false;
 };
+
+// TODO owner not defined
+const requestBook = () => {
+  // TODO
+  try {
+    const payload = {
+      bookId: props.book.id,
+      readerId: userStore.user.id,
+      ownerId: props.book.ownerId,
+      lendingStatus: LendingStatus.READER_CREATED_REQUEST
+    }
+    console.debug("Trying to create lending request with following payload: ", payload);
+    lendingService.createLending(payload);
+    Snackbar.showSnackbar('Lending Request successfully created!', SnackbarType.SUCCESS);
+  } catch(e) {
+    console.error("Error creating lending request", e);
+    Snackbar.showSnackbar('Unexpected error while creating lending request. Check console.', SnackbarType.ERROR);
+  }
+
+  hide();
+}
 
 defineExpose({
     show,
