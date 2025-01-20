ba57ab37a1a39edad19f01fcf859ba04afa2ae2c
djamn <david@jamnig.net>
2025-01-20 09:28:42 +0100

    readded token

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,7 +1,8 @@
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue'
+import { computed, ref } from 'vue';
 
 export const useAuthStore = defineStore('auth', () => {
+  const token = ref<string | null>(null);
   const isLoggedIn = computed(() => token.value !== null);
 
   const initializeToken = () => {
