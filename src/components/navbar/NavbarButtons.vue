d50b24aa6fdb91da727e8d2318ff4c905005ea78
djamn <david@jamnig.net>
2025-01-22 16:12:20 +0100

    fixed navbar on mobile devices

--- src/components/navbar/NavbarButtons.vue
+++ src/components/navbar/NavbarButtons.vue
@@ -3,13 +3,13 @@
     class="flex items-center flex-row justify-between space-x-6 max-lg:space-x-5 max-sm:space-x-6"
     ref="dropdownRef"
   >
-    <div v-if="loggedIn" class="tw-navbar-dropdown-container">
+    <div v-if="loggedIn" class="tw-navbar-dropdown-container max-sm:hidden">
       <router-link to="/my-books" class="navbar-button-container btn-primary">
         <font-awesome-icon class="tw-icon text-3xl max-sm:text-2xl" :icon="faBook" />
       </router-link>
     </div>
 
-    <div v-if="loggedIn" class="tw-navbar-dropdown-container">
+    <div v-if="loggedIn" class="tw-navbar-dropdown-container max-sm:hidden">
       <router-link to="/my-requests" class="navbar-button-container btn-primary">
         <font-awesome-icon class="tw-icon text-3xl max-sm:text-2xl" :icon="faMessage" />
       </router-link>
@@ -149,6 +149,26 @@
               :icon="faLocationDot"
             ></font-awesome-icon>
             My Location
+          </router-link>
+
+          <div class="tw-dropdown-separator"></div>
+
+          <router-link to="/my-books" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon
+              class="tw-navbar-dropdown-icon"
+              :icon="faBook"
+            ></font-awesome-icon>
+            My Books
+          </router-link>
+
+          <div class="tw-dropdown-separator"></div>
+
+          <router-link to="/my-requests" class="tw-dropdown-inner-action-layout">
+            <font-awesome-icon
+              class="tw-navbar-dropdown-icon"
+              :icon="faMessage"
+            ></font-awesome-icon>
+            My Requests
           </router-link>
 
           <div class="tw-dropdown-separator"></div>
